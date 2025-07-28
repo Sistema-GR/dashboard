344b71afe89503fffdf22c38be2a43376ff9fc4a
ABGarcez <arthur_b_garcez@hotmail.com>
2025-07-28 11:38:49 -0300

    Primeiras Alterações para a tela de recursos - NÃO FUNCIONAL AINDA

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,17 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+ // ADICIONE ESTE BLOCO DE CÓDIGO
+  server: {
+    proxy: {
+      // Qualquer requisição que comece com /api
+      '/api': {
+        // será redirecionada para o seu servidor Django
+        target: 'http://localhost:8000',
+        // Necessário para que o servidor de destino não recuse a requisição
+        changeOrigin: true,
+      },
+    }
   }
 })
