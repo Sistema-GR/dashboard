401bdd3cc143e8dd66894663dcbeed8a30c01edf
Giovana Gomes <2006.giovanaluiza.celular@gmail.com>
2025-08-05 10:42:40 -0300

    Update Sidebar component to improve icon and label visibility in minimized state

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -134,7 +134,7 @@
                                     @click="toggleCalcMenu"
                                   >
                                     <component :is="item.icon" class="h-auto w-6 shrink-0 stroke-white" aria-hidden="true" />
-                                    <span :class="isSidebarMinimized">{{ item.name }}</span>
+                                    <span :class="isSidebarMinimized ? 'hidden' : 'whitespace-nowrap'">{{ item.name }}</span>
                                     <ChevronDownIcon
                                       :class="['w-4 h-auto ml-auto transition-transform', isCalcMenuOpen ? 'rotate-180' : '', isSidebarMinimized ? 'hidden' : '']"
                                     />
@@ -182,7 +182,7 @@
                             xmlns="http://www.w3.org/2000/svg" 
                             viewBox="0 0 24 24" 
                             fill="currentColor" 
-                            :class="isSidebarMinimized ? 'w-8 h-auto' : 'w-10 h-auto'"
+                            :class="isSidebarMinimized ? 'w-6 h-auto' : 'w-10 h-auto'"
                             class="text-white"
                           >
                             <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clip-rule="evenodd" />
